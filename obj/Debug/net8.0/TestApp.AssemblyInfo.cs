7fab687ae77ca4aab993ba3084164ea538ada3b9
Danuja Dilanka <dilanka.rajapakshe1@gmail.com>
2024-09-21 21:01:22 +0530

    Ignore

--- obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc96d388108e4c8aaf224943c2fc47dba82e515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab5d9722940c5f308f52d0fc251c996d32c2325")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
