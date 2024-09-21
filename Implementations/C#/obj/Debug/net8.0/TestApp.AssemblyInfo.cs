39d4a42209d20c3b40748d93613294cd90e3b8bc
Danuja Dilanka <dilanka.rajapakshe1@gmail.com>
2024-09-21 20:09:13 +0530

    Initial

--- Implementations/C#/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ Implementations/C#/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a2a0ab0d8c5d1e8c4c60dfca603e3ad39b3fa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc96d388108e4c8aaf224943c2fc47dba82e515")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
