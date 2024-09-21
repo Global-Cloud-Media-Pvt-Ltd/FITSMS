9cc96d388108e4c8aaf224943c2fc47dba82e515
Danuja Dilanka <dilanka.rajapakshe1@gmail.com>
2024-09-21 20:06:08 +0530

    Initial

--- Implementations/C#/Program.cs
+++ Implementations/C#/Program.cs
@@ -12,9 +12,9 @@ namespace TestApp
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "https://app.fitsms.lk/api/v3/sms/send");
 
-            request.Headers.Add("Authorization", "Bearer 100|YqDF3DyliXqCd5IEjEjaa5lyTyWACXFBaZ2OVlCG");
+            request.Headers.Add("Authorization", "Bearer {{YOUR_API_KEY}}");
 
-            var content = new StringContent($"{{\"recipient\":\"{to}\",\"message\":\"{message}\",\"type\":\"plain\",\"sender_id\":\"FitSMS Demo\"}}", System.Text.Encoding.UTF8, "application/json");
+            var content = new StringContent($"{{\"recipient\":\"{to}\",\"message\":\"{message}\",\"type\":\"plain\",\"sender_id\":\"{{YOUR_MASK}}\"}}", System.Text.Encoding.UTF8, "application/json");
             request.Content = content;
 
             var response = await client.SendAsync(request);
@@ -29,7 +29,7 @@ namespace TestApp
         public static async Task Main(string[] args)
         {
             var smsService = new SmsService();
-            await smsService.SendSmsAsync("+94761695904", "Test Message");
+            await smsService.SendSmsAsync("{{YOUR_NUMBER_WITH_COUNTRY_CODE}}", "Test Message");
         }
     }
 }
--- Implementations/C#/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ Implementations/C#/obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a2a0ab0d8c5d1e8c4c60dfca603e3ad39b3fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
